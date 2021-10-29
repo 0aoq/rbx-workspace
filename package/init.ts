1cb11f5153a11db56ea300df4b22c82ce4c0b3a0
0aoq <73413330+0aoq@users.noreply.github.com>
2021-10-29 13:41:41 -0400

    Minor Fixes

--- package/init.ts
+++ package/init.ts
@@ -1,7 +1,15 @@
-import * as RBXWorkspace from './core/classes.js'
+// import * as RBXWorkspace from './core/classes.js'
+import { 
+    Part, 
+    game, 
+    RemoteEvent, 
+    CFrame, 
+    Udim2, 
+    Vector3 
+} from './core/classes.js'
 import { print, warn, error } from './core/luau.js'
 
-const ins = new RBXWorkspace.game({
+const ins = new game({
     ins: {
         Name: "game",
         Parent: "_G"
@@ -12,12 +20,12 @@ const GameObjects = ins.Objects()
 print(GameObjects)
 
 // follows type for part, vector3, and color3
-const test_part = new RBXWorkspace.Part({
+const test_part = new Part({
     ins: {
         Name: "Baseplate",
         Parent: "Workspace"
     },
-    Size: new RBXWorkspace.Vector3({x: 100, y: 5, z: 100}),
-    Position: new RBXWorkspace.Vector3({x: 0, y: 0, z: 0}),
-    Color: new RBXWorkspace.Color3({r: 15, g: 15, b: 15})
+    Size: new Vector3({x: 100, y: 5, z: 100}),
+    Position: new Vector3({x: 0, y: 0, z: 0}),
+    Color: new Color3({r: 15, g: 15, b: 15})
 })
